b83dbdf09859a8bce1d4f9dac2d3faac352a17bb
elinlager <elinlager@c-152-117.eduroam.liu.se>
2016-09-13 11:41:10 +0200

    Test

--- Audio.js
+++ Audio.js
@@ -1,4 +1,32 @@
 /**
  * Created by elinlager on 2016-09-13.
  */
+/** Add the audio file **/
 <audio id="myAudio" src="path-to-audio.mp3"></audio>
+
+
+/** Get the data from the AnalyserNode **/
+window.onload = function() {
+    var ctx = new AudioContext();
+    var audio = document.getElementById('myAudio');
+    var audioSrc = ctx.createMediaElementSource(audio);
+    var analyser = ctx.createAnalyser();
+    // we have to connect the MediaElementSource with the analyser
+    audioSrc.connect(analyser);
+    // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
+
+    // frequencyBinCount tells you how many values you'll receive from the analyser
+    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+    // we're ready to receive some data!
+    // loop
+    function renderFrame() {
+        requestAnimationFrame(renderFrame);
+        // update data in frequencyData
+        analyser.getByteFrequencyData(frequencyData);
+        // render frame based on values in frequencyData
+        // console.log(frequencyData)
+    }
+    audio.start();
+    renderFrame();
+};

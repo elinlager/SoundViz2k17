51f8ea22f63c8b886361e5ae9a7ae34490b5e36f
josefineflygge <josefineflygge@hotmail.com>
2016-09-16 09:27:11 +0200

    Gjorde någon slags bargraph

--- Audio.js
+++ Audio.js
@@ -9,16 +9,12 @@ myHeading.textContent = 'Hello world!';
 //size of canvas
 var WIDTH = 640;
 var HEIGHT = 100;
-
-var FFT_SIZE = 2048;
+var FFT_SIZE = 256;
 
 
 // set up canvas context for visualizer
 var canvas = document.querySelector('.visualizer');
 var canvasCtx = canvas.getContext("2d");
-
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var analyser = audioCtx.createAnalyser();
 
 /** Add the audio file **/
 var audio = new Audio();
@@ -27,7 +23,7 @@ audio.controls = true;
 audio.autoplay = true;
 document.querySelector('h2').appendChild(audio);
 
-var context = new webkitAudioContext();
+var context = new (window.AudioContext || window.webkitAudioContext)();
 var analyser = context.createAnalyser();
 
 window.addEventListener('load', function(e) {
@@ -40,43 +36,31 @@ window.addEventListener('load', function(e) {
 
 analyser.fftSize = FFT_SIZE;
 var bufferLength = analyser.frequencyBinCount;
+console.log(bufferLength);
 var dataArray = new Uint8Array(bufferLength);
-
-analyser.getByteTimeDomainData(dataArray);
-
 canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
 function draw() {
     drawVisual = requestAnimationFrame(draw);
 
-    analyser.getByteTimeDomainData(dataArray);
+    analyser.getByteFrequencyData(dataArray);
 
-    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-    canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-    canvasCtx.beginPath();
-
-    var sliceWidth = WIDTH * 1.0 / bufferLength;
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
     var x = 0;
 
     for(var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i]/2;
 
-        var v = dataArray[i] / 128.0;
-        var y = v * HEIGHT/2;
+        canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+        canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight);
 
-        if(i === 0) {
-            canvasCtx.moveTo(x, y);
-        } else {
-            canvasCtx.lineTo(x, y);
-        }
-
-        x += sliceWidth;
+        x += barWidth + 1;
     }
-    canvasCtx.lineTo(canvas.width, canvas.height/2);
-    canvasCtx.stroke();
 };
+
 
 draw();
